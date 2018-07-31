1890e9f26a7480bd614514c01ff794c2f2ed8865
黄志伟 <doggy1853@163.com>
2018-07-31 20:01:04 +0800

    multiple thread OOM

--- misctest/src/main/java/com/doggy/misctest/HeapOOM.java
+++ misctest/src/main/java/com/doggy/misctest/HeapOOM.java
@@ -5,15 +5,28 @@ import java.util.List;
 
 /**
  * @author doggy1853
- * VM args: -Xms20m -Xmx20m -XX:+HeapDumpOnOutOfMemoryError
+ * VM args: -Xms20m -Xmx20m -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/d/heap-dump/java.dump
  */
 public class HeapOOM {
-	static class OOMObject{}
-	
-	public static void main(String[] args) {
-		List<OOMObject> list = new ArrayList<>();
-		while (true){
-			list.add(new OOMObject());
-		}
-	}
+    static class OOMObject {
+    }
+
+    public static void main(String[] args) {
+        Thread t1 = new Thread(() -> {
+            List<OOMObject> list = new ArrayList<>();
+            while (true) {
+                list.add(new OOMObject());
+            }
+        });
+
+        Thread t2 = new Thread(() -> {
+            List<OOMObject> list = new ArrayList<>();
+            while (true) {
+                list.add(new OOMObject());
+            }
+        });
+
+        t1.start();
+        t2.start();
+    }
 }
