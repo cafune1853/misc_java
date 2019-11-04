0ad6eca3cf083370a8b4cef34d51dcb623afe0c0
doggy1853 <doggy1853@163.com>
2019-11-04 23:38:02 +0800

    完成BM字符串匹配的好后缀规则

--- algorithm/src/main/java/com/doggy/algorithm/StringMatch.java
+++ algorithm/src/main/java/com/doggy/algorithm/StringMatch.java
@@ -4,9 +4,9 @@ import java.util.*;
 
 public class StringMatch {
     public static void main(String[] args) {
-        String fullText = "full of text ";
-        String matchText1 = "te";
-        String matchText2 = "tx";
+        String fullText = "xxxxabxxxxxddddcabcab";
+        String matchText1 = "cabcab";
+        String matchText2 = "xxab";
         System.out.println(bruteForceMatch(fullText, matchText1));
         System.out.println(bruteForceMatch(fullText, matchText2));
         System.out.println(bmMatch(fullText, matchText1));
@@ -15,6 +15,9 @@ public class StringMatch {
 
     public static boolean bmMatch(CharSequence fullSeq, CharSequence matchSeq){
         Map<Character, ArrayList<Integer>> charIndexListMap = constructOrderedCharIndexListMap(matchSeq);
+        int[] suffix = new int[matchSeq.length()];
+        boolean[] matchHead = new boolean[matchSeq.length()];
+        setGoodSuffixAndMatchHead(matchSeq, suffix, matchHead);
         int start = 0;
         while(start <= fullSeq.length() - matchSeq.length()) {
             int i = matchSeq.length() - 1;
@@ -26,12 +29,52 @@ public class StringMatch {
             if (i < 0 ) {
                 return true;
             }
+            int lastMatchCount = matchSeq.length() - 1 - i;
+            int goodSuffixShouldMove = calculateMoveByGoodSuffix(matchSeq.length(), lastMatchCount, suffix, matchHead);
             // 坏字符匹配：右移匹配串直到找到一个字符和坏字符一致
             char badChar = fullSeq.charAt(start + i);
             int recentCorrectIndex = getRecentLastIndex(charIndexListMap, badChar, i);
-            start += (i - recentCorrectIndex);
+            int badCharShouldMove = i - recentCorrectIndex;
+            start += Math.max(goodSuffixShouldMove, badCharShouldMove);
         }
         return false;
+    }
+
+    private static int calculateMoveByGoodSuffix(int m, int mMatchedLength, int[] suffix, boolean[] matchHead){
+        if(mMatchedLength == 0){
+            return 1;
+        }
+        if(suffix[mMatchedLength] != -1){
+            return m - mMatchedLength - suffix[mMatchedLength];
+        }else{
+            for (int i = mMatchedLength; i >= 1; i--) {
+                if(matchHead[i]){
+                    return m - i;
+                }
+            }
+            return m;
+        }
+    }
+
+    private static void setGoodSuffixAndMatchHead(CharSequence cs, int[] suffix, boolean[] matchHead){
+        for (int i = 0; i < suffix.length - 1; i++) {
+            suffix[i] = -1;
+            matchHead[i] = false;
+        }
+        // 对cs[0, 0] ... cs[0, m - 2]中的每一个子串与cs求公共子后缀。
+        for (int i = 0; i <= cs.length() - 2; i++) {
+            int j = i;
+            for (; j >= 0; j--) {
+                if(cs.charAt(j) == cs.charAt(cs.length() - 1 - i + j)){
+                    suffix[i - j + 1] = j;
+                }else{
+                    break;
+                }
+            }
+            if(j == -1){
+                matchHead[i + 1] = true;
+            }
+        }
     }
 
     private static <T extends List<Integer> & RandomAccess> int getRecentLastIndex(Map<Character, T> characterListMap, char c, int lastIndex){
@@ -45,7 +88,7 @@ public class StringMatch {
 
     private static <T extends List<Integer> & RandomAccess> int binarySearchLastLessValue(T list, int start, int end, int value){
         if(start == end){
-            return list.get(start) < value ? list.get(start) : -1;
+            return list.get(start) < value ? start : -1;
         }
         int middle = start + (end - start) / 2;
         if(list.get(middle) >= value){
