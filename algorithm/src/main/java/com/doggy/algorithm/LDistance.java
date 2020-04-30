a78ab7b55c25452103735cf6bec86308cf49c6f2
huangzhw <doggy1853@163.com>
2020-04-30 10:22:59 +0800

    完成编辑距离

--- algorithm/src/main/java/com/doggy/algorithm/LDistance.java
+++ algorithm/src/main/java/com/doggy/algorithm/LDistance.java
@@ -2,7 +2,7 @@ package com.doggy.algorithm;
 
 public class LDistance {
     public static void main(String[] args) {
-        System.out.println(computeDistance("xxxs", "xxxy"));
+        System.out.println(computeDistance("xxxxsuuui", "xxxyuuuu"));
     }
 
     private static int computeDistance(String s1, String s2){
@@ -16,27 +16,23 @@ public class LDistance {
         }
         int[][] distance = new int[n+1][m+1];
         distance[0][0] = 0;
-        for (int j = 0; j < n + 1; j++){
-            int y = j;
-            int x = 0;
-            while (x < m + 1 && y >= 0){
-                if(x > 0 && y > 0){
+        for (int j = 0; j < m + 1; j++){
+            for (int i = 0; i < n + 1; i++) {
+                if(i > 0 && j > 0){
                     int min;
-                    if(s1.charAt(x - 1) == s2.charAt(y - 1)){
-                        min = distance[x - 1][y - 1];
+                    if(s1.charAt(i - 1) == s2.charAt(j - 1)){
+                        min = distance[i - 1][j - 1];
                     }else {
-                        min = distance[x - 1][y - 1] + 1;
+                        min = distance[i - 1][j - 1] + 1;
                     }
-                    min = Math.min(min, distance[x-1][y] + 1);
-                    min = Math.min(min, distance[x][y-1] + 1);
-                    distance[x][y] = min;
-                }else if(x > 0 && y == 0){
-                    distance[x][y] = distance[x-1][y] + 1;
-                }else if(y > 0 && x == 0){
-                    distance[x][y] = distance[x][y-1] + 1;
+                    min = Math.min(min, distance[i-1][j] + 1);
+                    min = Math.min(min, distance[i][j-1] + 1);
+                    distance[i][j] = min;
+                }else if(i > 0 && j == 0){
+                    distance[i][j] = distance[i-1][j] + 1;
+                }else if(j > 0 && i == 0){
+                    distance[i][j] = distance[i][j-1] + 1;
                 }
-                x ++;
-                y --;
             }
         }
         return distance[n][m];
